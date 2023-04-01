71eed702ddc23d0e747e2a5eb119599f315d95fe
ezaborszky <124446749+ezaborszky@users.noreply.github.com>
2023-04-01 20:14:54 +0200

    defined basic buttons

---
+++ script.js
@@ -0,0 +1,4 @@
+const allClearButton = document.querySelector("[data-allclear]")
+const deleteButton = document.querySelector("[data-delete]")
+const operatorButtons = document.querySelectorAll("[data-operator]")
+const numberButtons = document.querySelectorAll("[data-number]")
